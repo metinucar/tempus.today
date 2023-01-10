b07c6f709267fcb1f07cb4d5fc772e0260e58d3e
Metin Uçar <metin.ucar@tobii.com>
2023-01-10 19:56:03 +0100

    Adding typing effect and styling tweaks

--- script.js
+++ script.js
@@ -1,5 +1,92 @@
-// Sample array of quotes
-let quotes = [
+function getCurrentDayCount() {
+  const firstDateOfYear = new Date(new Date().getFullYear(), 0, 1);
+  const currentDate = new Date();
+  return Math.ceil(
+    ((currentDate - firstDateOfYear) / (1000 * 60 * 60 * 24)).toFixed(4)
+  );
+}
+
+function getCurrentWeekCount() {
+  const currentDate = new Date();
+  let tdt = new Date(currentDate.valueOf());
+  let dayn = (currentDate.getDay() + 6) % 7;
+  tdt.setDate(tdt.getDate() - dayn + 3);
+  let firstThursday = tdt.valueOf();
+  tdt.setMonth(0, 1);
+  if (tdt.getDay() !== 4) {
+    tdt.setMonth(0, 1 + ((4 - tdt.getDay() + 7) % 7));
+  }
+  return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+}
+
+function getCurrentProgress() {
+  const firstDateOfYear = new Date(new Date().getFullYear(), 0, 1);
+  const currentDate = new Date();
+  return (
+    (((currentDate - firstDateOfYear) / (1000 * 60 * 60 * 24)) * 100) /
+    365
+  ).toFixed(3);
+}
+
+function updateUI() {
+  // Day count
+  const dayCount = getCurrentDayCount();
+  const dayCountItem = document.getElementsByClassName("day-count")[0];
+  dayCountItem.textContent = `${dayCount}`;
+
+  // Week count
+  const weekCount = getCurrentWeekCount();
+  const weekCountItem = document.getElementsByClassName("week-count")[0];
+  weekCountItem.textContent = `${weekCount}`;
+
+  // Percentage progress
+  const percent = getCurrentProgress();
+  const percentPrefix = percent.split(".")[0];
+  const percentSuffix = percent.split(".")[1];
+  const barItem = document.getElementsByClassName("bar")[0];
+  barItem.style.width = `${percent}%`;
+
+  const counterItemPrefix = document.getElementsByClassName("value-prefix")[0];
+  counterItemPrefix.textContent = `${percentPrefix}`;
+
+  const counterItemSuffix = document.getElementsByClassName("value-suffix")[0];
+  counterItemSuffix.textContent = `${percentSuffix}`;
+
+  // Year
+  const currentYear = new Date().getFullYear();
+  const yearTextItem = document.getElementsByClassName("year")[0];
+  yearTextItem.textContent = currentYear;
+
+  // Date
+  const currentDate = new Date();
+  const dateItem = document.getElementsByClassName("date")[0];
+  const dateItemOptions = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+  };
+  dateItem.textContent = currentDate.toLocaleDateString(
+    window.navigator.language,
+    dateItemOptions
+  );
+
+  // Time
+  const timeItem = document.getElementsByClassName("time")[0];
+  const timeItemOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+  timeItem.textContent = currentDate.toLocaleTimeString(
+    window.navigator.language,
+    timeItemOptions
+  );
+}
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [
   "Time flies like an arrow, but fruit flies like a banana.",
   "Time is a jet plane, it moves too fast.",
   "Time is a thief, it steals moments we can never get back.",
@@ -156,97 +243,40 @@ let quotes = [
   "Time is like a big clock, ticking and tocking all through the day.",
   "Time is like a big, giant wave, crashing over us and carrying us along.",
 ];
+const typingDelay = 100;
+const erasingDelay = 25;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = Math.floor(Math.random() * textArray.length);
+let charIndex = 0;
 
-function getCurrentDayCount() {
-  const firstDateOfYear = new Date(new Date().getFullYear(), 0, 1);
-  const currentDate = new Date();
-  return Math.ceil(
-    ((currentDate - firstDateOfYear) / (1000 * 60 * 60 * 24)).toFixed(4)
-  );
-}
-
-function getCurrentWeekCount() {
-  const currentDate = new Date();
-  let tdt = new Date(currentDate.valueOf());
-  let dayn = (currentDate.getDay() + 6) % 7;
-  tdt.setDate(tdt.getDate() - dayn + 3);
-  let firstThursday = tdt.valueOf();
-  tdt.setMonth(0, 1);
-  if (tdt.getDay() !== 4) {
-    tdt.setMonth(0, 1 + ((4 - tdt.getDay() + 7) % 7));
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay + 7000);
   }
-  return 1 + Math.ceil((firstThursday - tdt) / 604800000);
 }
 
-function getCurrentProgress() {
-  const firstDateOfYear = new Date(new Date().getFullYear(), 0, 1);
-  const currentDate = new Date();
-  return (
-    (((currentDate - firstDateOfYear) / (1000 * 60 * 60 * 24)) * 100) /
-    365
-  ).toFixed(3);
-}
-
-function updateUI() {
-  // Day count
-  const dayCount = getCurrentDayCount();
-  const dayCountItem = document.getElementsByClassName("day-count")[0];
-  dayCountItem.textContent = `${dayCount}`;
-
-  // Week count
-  const weekCount = getCurrentWeekCount();
-  const weekCountItem = document.getElementsByClassName("week-count")[0];
-  weekCountItem.textContent = `${weekCount}`;
-
-  // Percentage progress
-  const percent = getCurrentProgress();
-  const percentPrefix = percent.split(".")[0];
-  const percentSuffix = percent.split(".")[1];
-  const barItem = document.getElementsByClassName("bar")[0];
-  barItem.style.width = `${percent}%`;
-
-  const counterItemPrefix = document.getElementsByClassName("value-prefix")[0];
-  counterItemPrefix.textContent = `${percentPrefix}`;
-
-  const counterItemSuffix = document.getElementsByClassName("value-suffix")[0];
-  counterItemSuffix.textContent = `${percentSuffix}`;
-
-  // Year
-  const currentYear = new Date().getFullYear();
-  const yearTextItem = document.getElementsByClassName("year")[0];
-  yearTextItem.textContent = currentYear;
-
-  // Date
-  const currentDate = new Date();
-  const dateItem = document.getElementsByClassName("date")[0];
-  const dateItemOptions = {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-  };
-  dateItem.textContent = currentDate.toLocaleDateString(
-    window.navigator.language,
-    dateItemOptions
-  );
-
-  // Time
-  const timeItem = document.getElementsByClassName("time")[0];
-  const timeItemOptions = {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  };
-  timeItem.textContent = currentDate.toLocaleTimeString(
-    window.navigator.language,
-    timeItemOptions
-  );
-}
-
-function displayRandomQuote(quotes) {
-  let index = Math.floor(Math.random() * quotes.length);
-  let quote = quotes[index];
-  const quotesElement = document.getElementsByClassName("quotes-container")[0];
-  quotesElement.textContent = `${quote}`;
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex = Math.floor(Math.random() * textArray.length);
+    setTimeout(type, typingDelay);
+  }
 }
 
 setInterval(
@@ -257,11 +287,7 @@ setInterval(
   1000
 );
 
-displayRandomQuote(quotes);
-setInterval(
-  (function () {
-    displayRandomQuote(quotes);
-    return arguments.callee;
-  })(),
-  15000
-);
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, 300);
+});
